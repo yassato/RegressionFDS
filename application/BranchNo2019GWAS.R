d970fd0a75a4ab1a45e45359d9b5e975a7460ec4
Yasuhiro Sato <yassato36c@outlook.com>
2021-06-11 15:52:19 +0200

    Add files via upload

--- application/BranchNo2019GWAS.R
+++ application/BranchNo2019GWAS.R
@@ -3,8 +3,6 @@
 #####################
 
 library(rNeighborGWAS)
-source("nei_lmm.R")
-source("nei_lm.R")
 
 #load data
 geno_d = readRDS(file="sub_snpMAF5.rds")
@@ -12,7 +10,7 @@ geno_d[geno_d==0]=-1 #replace 0 into -1
 
 position = readRDS(file="positionsMAF5.rds")
 
-pheno_d = read.csv("./Survey2019CHZ4GWAS_BranchNo.csv",header=T)
+pheno_d = read.csv("./pheno_branchNum.csv",header=T)
 pheno_d$gwasID = paste0("X",pheno_d$gwasID)
 
 #reshape pheno.data
@@ -38,27 +36,30 @@ g_nei = nei_coval(geno=geno,smap=smap,scale=scale,grouping=pheno_d$Block)
 X = as.matrix(model.matrix(~factor(Block)+scale(InitLeafLen)+Bolting+edge-1,data=pheno_d))
 Y = scale(log(pheno_d$BranchNo+1))
 
+# LMM w/ asymmetric FDS
 res = nei_lmm(geno=geno,g_nei=g_nei,pheno=Y,addcovar=X,n_core=24L,asym=TRUE)
 res = data.frame(position,res)
 colnames(res) = c("Chr","Position","MAF","beta_self","beta_nei","beta_sxn","P_self","P_nei","P_sxn")
 write.csv(res,"210604_BranchNoNeiGWASbolting_CHZ2019_scaledLMMasym.csv")
 gc();gc()
 
+# LM w/ asymmetric FDS
 res = nei_lm(geno=geno,g_nei=g_nei,pheno=Y,addcovar=X,n_core=24L,asym=TRUE)
 res = data.frame(position,res)
 colnames(res) = c("Chr","Position","MAF","beta_self","beta_nei","beta_sxn","P_self","P_nei","P_sxn")
 write.csv(res,"210604_BranchNoNeiGWASbolting_CHZ2019_scaledLMasym.csv")
 gc();gc()
 
+# LMM w/o asymmetric FDS
 res = nei_lmm(geno=geno,g_nei=g_nei,pheno=Y,addcovar=X,n_core=24L)
 res = data.frame(position,res)
 colnames(res) = c("Chr","Position","MAF","beta_self","beta_nei","P_self","P_nei")
 write.csv(res,"210604_BranchNoNeiGWASbolting_CHZ2019_scaledLMM.csv")
 gc();gc()
 
+# LMM w/o asymmetric FDS
 res = nei_lm(geno=geno,g_nei=g_nei,pheno=Y,addcovar=X,n_core=24L)
 res = data.frame(position,res)
 colnames(res) = c("Chr","Position","MAF","beta_self","beta_nei","P_self","P_nei")
 write.csv(res,"210604_BranchNoNeiGWASbolting_CHZ2019_scaledLM.csv")
 gc();gc()
-
